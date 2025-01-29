04f3928566ef89d1c9725016950ee4e779cebd60
DiogoBrazil <diogoifroads@gmail.com>
2025-01-29 06:08:39 -0400

    update: response file updated

--- src/utils/response.rs
+++ src/utils/response.rs
@@ -58,6 +58,14 @@ impl<T: Serialize> ApiResponse<T> {
         }
     }
 
+    pub fn updated_password() -> Self {
+        Self {
+            message: "Resource updated successfully".to_string(),
+            status_code: StatusCode::OK,
+            data: None,
+        }
+    }
+
     pub fn deleted() -> Self {
         Self {
             message: "Resource deleted successfully".to_string(),
